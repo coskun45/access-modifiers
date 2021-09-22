8722e9e585944e1f283d5d2e15d60987be57ec7d
ecoskun <67566471+coskun45@users.noreply.github.com>
2021-09-22 14:13:54 +0200

    abstract class

---
+++ Abstract Class ve Interface lerin farki/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Abstract_Class_ve_Interface_lerin_farki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
